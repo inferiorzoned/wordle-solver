cdab1118709a3c9891c943d5fd57b7ee9c907b23
inferiorzoned <1605009@ugrad.cse.buet.ac.bd>
2022-05-05 22:50:19 +0000

    more benchmarking added

--- solver/benchmarking.py
+++ solver/benchmarking.py
@@ -5,13 +5,17 @@ allWordleWords = open("included.txt").read().splitlines()
 wordAttemptDict = {}
 attemptCount = 0
 attemptSum = 0
+correctlyGuessed = 0
 for word in allWordleWords:
     solver = WordleSolver()
     solver.testSolver(word)
     wordAttemptDict[word] = solver.attemptsTaken
     attemptSum += solver.attemptsTaken
     attemptCount += 1
+    if solver.attemptsTaken <= 6:
+        correctlyGuessed += 1
 print(wordAttemptDict)
 print(f"Total number of attempts are {attemptSum}")
 print(f"Number of wordle words are {attemptCount}")
 print(f"Average attempt is {attemptSum/attemptCount}")
+print(f"Number of words guessed within 6 attempts: {correctlyGuessed}")
