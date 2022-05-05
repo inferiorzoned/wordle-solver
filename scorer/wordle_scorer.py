a666863acf40c03906d5b9ddf8cb640afd6298bf
inferiorzoned <mdhasibulhussainh@gmail.com>
2022-05-06 04:48:36 +0600

    comments oraganized

--- scorer/wordle_scorer.py
+++ scorer/wordle_scorer.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
 def scoreAWord(word):
+    """
+    Scores a word according to the scoring values of the letters
+    : param word: the word to be scored
+    """
     # letterPosFreqScores: 5 rows (positions), 26 columns (letters)
     letterPosFreqScores = pd.read_csv('../scorer/scoring utils/weighted_freqs_letters_positions.csv')
     # letterAppearFreqScores: 26 columns (letters)
@@ -33,6 +37,9 @@ class WordleScorer:
         self.validWords = validWords
 
     def scoreAllWords(self):
+        """
+        Scores all the words in the wordlist
+        """
         self.wordScores = {}
         for word in self.validWords:
             self.wordScores[word] = scoreAWord(word)
